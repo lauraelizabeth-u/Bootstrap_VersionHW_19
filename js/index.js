afcd419e7cdf0d98f975f4783e4efb7297994939
lauraelizabeth-u <94931914+lauraelizabeth-u@users.noreply.github.com>
2022-05-04 14:51:57 -0400

    thank u hooman

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// console.log("your index.js file is working !");
+
+$(".col.my-col2").on("click", function(){
+    $(".col.my-col2").css("background-color","");
+    $(this).css("background-color","red");
+});
+
